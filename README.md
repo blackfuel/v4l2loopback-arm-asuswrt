31d7b665707647f536f5297119115cebb0f5c93e
blackfuel <blackfuel@github.com>
2017-04-23 19:54:21 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# v4l2loopback-arm-asuswrt
+
+### HOWTO: Patch to add v4l2loopback kernel option to AsusWRT firmware
+```
+cd
+git clone https://github.com/blackfuel/v4l2loopback-arm-asuswrt.git
+cd v4l2loopback-arm-asuswrt
+./v4l2loopback.sh
+```
+
---
+++ v4l2loopback.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#############################################################################
+# v4l2loopbackup for AsusWRT
+#
+# This script is a kernel patch for adding v4l2loopbackup capability to 
+# Asus ARM routers.
+#
+# Location of v4l2loopback kernel modules after building AsusWRT:
+# ~/asuswrt-merlin/release/src/router/arm-uclibc/target/lib/modules/2.6.36.4brcmarm/kernel/drivers/media
+#
+#############################################################################
+PATH_CMD="$(readlink -f $0)"
+
+set -e
+set -x
+
+# directories
+MERLINDIR="~/asuswrt-merlin"
+PACKAGEDIR="~/v4l2loopback"
+
+# download v4l2loopback
+cd
+git clone https://github.com/umlaeute/v4l2loopback.git
+
+# apply patch to add v4l2loopback kernel option
+pushd .
+cd $MERLINDIR
+PATCH_NAME="${PATH_CMD%/*}/asuswrt-arm-v4l2loopback.patch"
+patch --dry-run --silent -p2 -i "$PATCH_NAME" >/dev/null 2>&1 && \
+  patch -p2 -i "$PATCH_NAME" || \
+  echo "The Linux kernel patch was not applied."
+popd
+
+# create symbolic links in Asuswrt-Merlin to the source files in v4l2loopback project
+for KERNELDIR in "src-rt-6.x.4708" "src-rt-7.14.114.x/src" "src-rt-7.x.main/src"; do
+  [ ! -h $MERLINDIR/release/$KERNELDIR/linux/linux-2.6.36/drivers/media/video/v4l2loopback.c ] && ln -sf $PACKAGEDIR/v4l2loopback.c $MERLINDIR/release/$KERNELDIR/linux/linux-2.6.36/drivers/media/video/v4l2loopback.c
+  [ ! -h $MERLINDIR/release/$KERNELDIR/linux/linux-2.6.36/drivers/media/video/v4l2loopback_formats.h ] && ln -sf $PACKAGEDIR/v4l2loopback_formats.h $MERLINDIR/release/$KERNELDIR/linux/linux-2.6.36/drivers/media/video/v4l2loopback_formats.h
+done
+
